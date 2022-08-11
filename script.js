b55fac12f8cc984b62640f6d0a9ef80530177c32
David Irwin <redirwin@gmail.com>
2022-08-10 18:30:37 -0700

    refactored JS

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-const slideContainer = document.querySelector(".slide-container");
 const slides = document.querySelectorAll(".slide-container");
+const progressButtons = document.querySelectorAll(".progress-button");
 const indexButtonsContainer = document.querySelector(
   ".index-buttons-container"
 );
 const indexButtons = indexButtonsContainer.childNodes;
-const progressButtons = document.querySelectorAll(".progress-button");
 let activeSlide = 0;
 
-function setupCarouselButtons() {
-  slides[0].classList.add("active");
-  slides.forEach(() => {
-    indexButtonsContainer.innerHTML += `<button class="index-button"></button>`;
-  });
-  indexButtons[0].classList.add("active");
-}
+slides[0].classList.add("active"); // Adds active class to first slide.
 
-function handleProgressButtonClicks() {
-  progressButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      index === 0 ? (activeSlide -= 1) : activeSlide++;
-      activeSlide < 0 && (activeSlide = slides.length - 1);
-      activeSlide > slides.length - 1 && (activeSlide = 0);
+// Generates an index button for each slide...
+slides.forEach(() => {
+  indexButtonsContainer.innerHTML += `<button class="index-button"></button>`;
+});
+indexButtons[0].classList.add("active"); // ...and adds active class to the first one.
+
+// Handles clicks on next and previous buttons.
+progressButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    index === 0 ? (activeSlide -= 1) : activeSlide++;
+    activeSlide < 0 && (activeSlide = slides.length - 1);
+    activeSlide > slides.length - 1 && (activeSlide = 0);
+    changeSlide();
+  });
+});
+
+// Handles clicks on index buttons
+indexButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (index !== activeSlide) {
+      activeSlide = index;
       changeSlide();
-    });
+    }
   });
-}
+});
 
-function handleIndexButtons() {
-  indexButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      if (index !== activeSlide) {
-        activeSlide = index;
-        changeSlide();
-      }
-    });
-  });
-}
-
+// Checks activeSlide after any button click and changes current slide to match.
 function changeSlide() {
   slides.forEach((slide, index) => {
     index === activeSlide
@@ -49,7 +47,3 @@ function changeSlide() {
       : button.classList.remove("active");
   });
 }
-
-setupCarouselButtons();
-handleProgressButtonClicks();
-handleIndexButtons();
